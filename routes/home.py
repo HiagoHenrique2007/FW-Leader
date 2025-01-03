abdfab343b715916bd1cb73dd6779ddb6c85e754
HiagoHenrique2007 <imHiagoHenrique@gmail.com>
2025-01-03 19:47:22 -0300

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -5,7 +5,6 @@ import sqlite3 as sql
 home = Blueprint('HOME', __name__)
 
 db = './../db/depoimentos.db'
-global db
 create_table = '''
     CREATE TABLE IF NOT EXISTS depoimentos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
